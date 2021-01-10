a34678617fd545d8ccafbafd7a38851278e1f96b
Lareena <lareena.e.llamado@gmail.com>
2021-01-09 22:17:04 -0500

    commit

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,51 +1,63 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Store our USGS Earthquake API endpoint inside quakeUrl
+var quakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Store our Tectonic Plate API endpoint inside platesUrl
+var platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+// var platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
 
 
+// Perform a GET request to the quake URL
+d3.json(quakeUrl, function(data) {
+  // Store earthquake JSON response object into variable
+  let earthquakeData = data.features
+  // Perfom a GET request to the plates URL inside other d3.json so both objects within scope
+  d3.json(platesUrl, function(data) {
+    // Store plate tectonic JSON response object into variable
+    let platesData = data.features
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createMap function
-  createMap(data.features);
-});
+    // Call createMap function and pass both JSON response objects
+    createMap(earthquakeData,platesData)
+  })
+})
+
+// Function to Create Map 
+function createMap(earthquakeData,platesData) {
+
+    // Create Markers for each earthquakeData feature
+    let earthquakeMarkers = earthquakeData.map((feature) =>
+      // Make circleMarker and bind Popup for each earthquakeData feature
+      L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
+          radius: magCheck(feature.properties.mag),  // Call magCheck function to eliminate values <= 0 and scale
+          stroke: true,
+          color: 'black',
+          opacity: 1,
+          weight: 0.5,
+          fill: true,
+          fillColor: magColor(feature.properties.mag),  // Call magColor function to color marker according to magnitude
+          fillOpacity: 0.9   
+      })
+      .bindPopup("<h1> Magnitude : " + feature.properties.mag +
+      "</h1><hr><h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>")
+    )
+
+    // Create layerGroup for earthquakeMarkers
+    let earthquakes = L.layerGroup(earthquakeMarkers);
 
 
-function createMap(earthquakeData) {
-
-    // Loop through locations and markers elements
-      EarthquakeMarkers = earthquakeData.map((feature) =>
-                    //Yes, the geojson 'FORMAT' stores it in reverse, for some reason. (L.geojson parses it as [lat,lng] for you)
-                     //lat                         //long  
-        L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
-            radius: magCheck(feature.properties.mag),
-            stroke: true,
-            color: 'black',
-            opacity: 1,
-            weight: 0.5,
-            fill: true,
-            fillColor: magColor(feature.properties.mag),
-            fillOpacity: 0.9   
-        })
-        .bindPopup("<h1> Magnitude : " + feature.properties.mag +
-        "</h1><hr><h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>")
-      )
-
-      // Add the earthquakes layer to a marker cluster group.
-      var earthquakes=L.layerGroup(EarthquakeMarkers)
-    //    console.log(d3.extent(d3.values(earthquakeData,((d) => +d.properties.mag))));
-       var mags = earthquakeData.map((d) => magCheck(+d.properties.mag));
-       console.log(d3.extent(mags));
-       console.log(mags);
-    //    console.log(earthquakeData.properties.mag);
+    function makePolyline(feature, layer){
+      L.polyline(feature.geometry.coordinates);
+    }
+    
+    let plates = L.geoJSON(platesData, {
+      onEachFeature: makePolyline,
+        style: {
+          color: 'red',
+          opacity: 0.9
+        }
+    })
 
   
-
-
-
-
-
-  // Define streetmap and darkmap layers
+  // Define streetmap, darkmap, satellite and outdoors layers
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -53,30 +65,45 @@ function createMap(earthquakeData) {
     accessToken: API_KEY
   });
 
-//   var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.dark",
-//     accessToken: API_KEY
-//   });
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
 
-//   // Define a baseMaps object to hold our base layers
-//   var baseMaps = {
-//     "Street Map": streetmap,
-//     "Dark Map": darkmap
-//   };
+  var satellite =  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
 
-//   // Create overlay object to hold our overlay layer
-//   var overlayMaps = {
-//     Earthquakes: earthquakes
-//   };
+  var outdoors =  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.outdoors",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap,
+    "Satellite Map": satellite,
+    "Outdoors Map": outdoors
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes,
+    Plates : plates
+  };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
+    center: [37.09, -95.71],
+    zoom: 3,
     layers: [streetmap, earthquakes]
   });
 
@@ -96,15 +123,16 @@ legend.onAdd = function(myMap){
 }
 
 legend.addTo(myMap);
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
+  //Create a layer control
+  //Pass in our baseMaps and overlayMaps
+  //Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
 }
+
 /* Define a function to assign a color to the magnitude of the earthquake.
-   
+
 
      function magColor(mag) {
       var color = "";
@@ -119,9 +147,10 @@ legend.addTo(myMap);
     };
 // Function to determine if the magnitude is zero or less (See above discussion as it is possible to have
 // negative magnitudes, which obviously can't be used for setting the circleMarker radius)
+
 function magCheck(mag){
   if (mag <= 1){
-      return 8
+      return 6
   }
-  return mag * 8;
+  return mag * 6;
 }
